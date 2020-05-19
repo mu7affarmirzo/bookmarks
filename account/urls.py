689d0df86a6d9e2e9c7ae10ded33b7ea8466a2d8
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2020-05-19 05:15:55 +0500

    registration added

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -31,4 +31,7 @@ urlpatterns = [
 		auth_views.PasswordResetCompleteView.as_view(),
 		name='password_reset_complete'
 	),
+	path('', include('django.contrib.auth.urls')),
+
+	path('register/', views.register, name='register'),
 ]
