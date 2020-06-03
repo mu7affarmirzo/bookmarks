47a17fd65c46e9ba1e111ab56f57d731bf456645
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2020-06-04 03:16:51 +0500

    created a detail view for images with failure, next is creating image thumbnails

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 class ImageModel(models.Model):
     user = models.ForeignKey(
@@ -28,3 +29,5 @@ class ImageModel(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super(ImageModel, self).save(*args, **kwargs)
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
--- images/views.py
+++ images/views.py
@@ -3,6 +3,16 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 
+from django.shortcuts import get_object_or_404
+from .models import ImageModel
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(ImageModel, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section':'images',
+                   'image':image})
+
 @login_required
 def image_create(request):
     if request.method == 'POST':
