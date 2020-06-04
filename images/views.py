009294730794e98d2766d442033cb34b3cb7d74c
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2020-06-04 07:02:32 +0500

    style fixed and errors too

--- images/views.py
+++ images/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from django.shortcuts import get_object_or_404
 from .models import ImageModel
@@ -16,9 +18,9 @@ def image_detail(request, id, slug):
 @login_required
 def image_create(request):
     if request.method == 'POST':
-        form = ImageCreateForm(date=request.POST)
+        form = ImageCreateForm(data=request.POST)
         if form.is_valid():
-            cd = form.clean_date
+            cd = form.cleaned_data
             new_item = form.save(commit=False)
 
             new_item.user = request.user
@@ -28,3 +30,28 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
 
     return render(request, 'images/image/create.html',{'section': 'images','form': form})
+
+@login_required
+def image_list(request):
+    images = ImageModel.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request,
+                  'images/image/list.html',
+                   {'section': 'images', 'images': images})
